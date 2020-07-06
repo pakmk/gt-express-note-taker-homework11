165fc3badfa58330d5ecfcc35222eddc2311dd3d
min <pakmk8@gmail.com>
2020-07-06 17:20:08 -0400

    updated notes.html

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-  // 1. Require express
+// 1. Require express
 const express = require("express");
+// 6. Create a path
 const path = require("path");
 // 2. Create an instance of express called app
 const app = express();
@@ -10,9 +11,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// View / HTML
+// 7. View / HTML
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  //   res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  // res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 // API / JSON
