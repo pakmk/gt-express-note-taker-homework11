ebbeb7023daac7f8e220050389abe0e06447508d
min <pakmk8@gmail.com>
2020-07-07 20:37:08 -0400

    updated api get post

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const path = require("path");
 const app = express();
 // 3. Add a PORT
 const PORT = process.env.PORT || 3000;
+// Require database and filesystem
+const database = require("./db/db.json");
+const fs = require("fs");
 
 // 5. Add data processing for POST routes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // 7. View / HTML
 app.get("/", (req, res) => {
@@ -23,6 +27,26 @@ app.get("/notes", (req, res) => {
 });
 
 // API / JSON
+
+app.get("/api/notes", (req, res) => {
+  return res.json(database);
+});
+
+app.post("/api/notes", (req, res) => {
+  database.push(req.body);
+  fs.writeFile(path.join(__dirname, "./db/db.json"),JSON.stringify(database),(err) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log("Success!");
+      }
+    });
+  return res.json(database);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 // 4. Listen on that PORT
 app.listen(PORT, (req, res) => {
